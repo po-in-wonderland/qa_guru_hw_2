561fb822e7be4991635fb4e30127f365ae6c93e8
po-in-wonderland <55464609+po-in-wonderland@users.noreply.github.com>
2023-02-28 00:47:58 +0300

    Update Main.java

--- src/main/java/rt/kulagina/Main.java
+++ src/main/java/rt/kulagina/Main.java
@@ -2,6 +2,6 @@ package rt.kulagina;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("HELLO WORLD!!!");
     }
 }
